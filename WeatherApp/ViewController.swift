da9216b4503c771d5d5a16e2244f89a146ad6746
Gudla Srinivas <gsrinivas37@gmail.com>
2014-10-18 12:37:56 -0700

    Initial Commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var city: UITextField!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func getWeather(sender: AnyObject) {
+        var trimmedCity = city.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        trimmedCity = (trimmedCity as NSString).stringByReplacingOccurrencesOfString(" ", withString: "")
+        var urlStr = "http://www.weather-forecast.com/locations/\(trimmedCity)/forecasts/latest"
+    
+        var url = NSURL(string: urlStr)
+        
+        var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+            var urlContent: NSString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+            if(urlContent.containsString("<span class=\"phrase\">")) {
+                var array = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+            
+                var weather = (array[1].componentsSeparatedByString("</span>")[0]) as NSString
+            
+            
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.message.text = weather.stringByReplacingOccurrencesOfString("&deg;", withString:"°")
+                }
+            }else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.message.text = "City not found. Please try another one!"
+                }
+            }
+        })
+        
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
